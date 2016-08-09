297f7fdf5b2f87ced11b4131201de661e8ea9d4d
Jaswant Singh <jaswant.sandhu88@gmail.com>
2016-08-09 22:57:19 +0530

    Removed product list.

--- src/app/components/home.ts
+++ src/app/components/home.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { ProductList } from './product-list';
 
 @Component({
 	selector: 'home',
 	templateUrl: '/resources/views/home.html',
-	directives : [ProductList],
+	directives : [],
 	styleUrls : ["resources/stylesheets/home.css"]
 })
 export class Home {
